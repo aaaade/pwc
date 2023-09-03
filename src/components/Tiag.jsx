3ae939a89434ae94fc1d036a49de2bb8da97a373
Mishael <Mishaeldada@gmail.com>
2023-09-03 06:19:56 +0100

    fixed tiag

---
+++ src/components/Tiag.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import tiag from "../images/tiag.png"
+
+function Tiag() {
+  return (
+    <div className='bg-[#F8F9FA] py-[3rem] mt-[1rem]'>
+        <div className='w-[18rem] mx-auto'>
+            <img src={tiag} alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default Tiag
